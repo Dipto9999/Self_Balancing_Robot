7481e675f1920154c336e26161bc23495430dea9
Muntakim Rahman <muntakim.rahman@gmail.com>
2025-02-09 18:04:28 -0800

    Switched AIN, BIN by Board

--- Firmware/main/pwm.ino
+++ Firmware/main/pwm.ino
@@ -1,10 +1,10 @@
 #include "pwm.h"
 
 // Use the PwmOut Interface to Control Frequency and Duty Cycle.
-mbed::PwmOut PinAIN1(digitalPinToPinName(3)); // Pin 3
-mbed::PwmOut PinAIN2(digitalPinToPinName(5)); // Pin 5
-mbed::PwmOut PinBIN1(digitalPinToPinName(6)); // Pin 6
-mbed::PwmOut PinBIN2(digitalPinToPinName(9)); // Pin 9
+mbed::PwmOut PinAIN1(digitalPinToPinName(6)); // Pin 6
+mbed::PwmOut PinAIN2(digitalPinToPinName(9)); // Pin 9
+mbed::PwmOut PinBIN1(digitalPinToPinName(3)); // Pin 3
+mbed::PwmOut PinBIN2(digitalPinToPinName(5)); // Pin 5
 
 void setupPWM() {
     PinAIN1.period_us(20); // PWM Period of 20µs (50kHz)
@@ -12,7 +12,7 @@ void setupPWM() {
     PinBIN1.period_us(20); // PWM Period of 20µs (50kHz)
     PinBIN2.period_us(20); // PWM Period of 20µs (50kHz)
 
-    moveForwardSlowDecay(PinAIN1, 0.5);
+    // moveForwardSlowDecay(PinAIN1, 0.5);
     moveReverseSlowDecay(PinBIN1, 0.5);
 }
 
