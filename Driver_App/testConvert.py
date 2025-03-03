d2eb86990f059ac87fd578b9628d6a2c9a65796d
Muntakim Rahman <muntakim.rahman@gmail.com>
2025-03-03 12:35:24 -0800

    Adding Test File for Debugging

---
+++ Driver_App/testConvert.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+def convert_video(filename):
+    input_file = f"{filename}.h264"
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    input_file = os.path.join(base_dir, input_file)
+
+    # Output File Paths
+    mp4_file = os.path.join(base_dir, filename + ".mp4")
+    temp_file = os.path.join(base_dir, "converted.mp4")
+
+    # Flip Video Vertically + Horizontally and Copy Audio
+    command = [
+        "ffmpeg", # Command
+        "-i", input_file, # Input File
+        "-vf", "vflip,hflip", # Vertical and Horizontal Flip
+        "-c:a", "copy", # Copy Audio
+        temp_file
+    ]
+
+    print(input_file)
+
+    try:
+        result = subprocess.run(command, check = True, capture_output = True, text = True) # Run Command
+        print("ffmpeg Output:", result.stdout)
+        os.replace(temp_file, mp4_file)  # Replace the Temporary File with the MP4 File
+        os.remove(input_file) # Remove the original H264 File
+        print(f"Conversion Successful. Video Saved as {mp4_file}")
+    except subprocess.CalledProcessError as e:
+        print("ffmpeg Conversion Failed:", e)
+    except OSError as e:
+        print("File Operation Failed:", e)
+
+convert_video("Camera_Data_20250303_122913")
