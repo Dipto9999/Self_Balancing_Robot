fcab28f49cc4528a78b3e0d3c471974e274aaf15
Muntakim Rahman <muntakim.rahman@gmail.com>
2025-04-15 03:33:34 -0700

    Login Page Implemented

--- Driver_App/webApp.py
+++ Driver_App/webApp.py
@@ -1,8 +1,13 @@
+import os
+from functools import wraps
+from dotenv import load_dotenv
+from datetime import timedelta
+
 import asyncio
-import threading
+import threading as td
 
 from bleak import BleakScanner, BleakClient, BleakError
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 
 class BluetoothManager:
     # Define UUIDs for BLE Service and Characteristic
@@ -19,7 +24,7 @@ class BluetoothManager:
         self.start_loop()
 
     def start_loop(self):
-        threading.Thread(target = self._run_loop, daemon = True).start()
+        td.Thread(target = self._run_loop, daemon = True).start()
 
     def _run_loop(self):
         asyncio.set_event_loop(self.loop)
@@ -31,7 +36,6 @@ class BluetoothManager:
     async def scan(self):
         self.devices = []
         scanned = await BleakScanner.discover()
-
         self.devices = [
             {"name" : device.name, "address" : device.address} for device in scanned if device.name and ("WALL-E" in device.name)
         ] # Filter BLE Devices
@@ -42,7 +46,6 @@ class BluetoothManager:
         if self.client and self.client.is_connected:
             self.is_connected = True
             return True
-
         self.client = BleakClient(address)
         try:
             await self.client.connect()
@@ -68,25 +71,59 @@ class BluetoothManager:
             return True, f"Sent Command: {cmd}"
         except BleakError as e:
             return False, f"Error Sending Command: {e}"
-
 class RobotDriverApp:
     def __init__(self):
         self.app = Flask("Robot Driver App")
+        self.app.secret_key = os.environ.get("FLASK_SECRET_KEY", "fallback_dev_key")
+
+        self.app.config['SESSION_PERMANENT'] = False
+        self.app.permanent_session_lifetime = timedelta(minutes = 15)
+
         self.bluetooth_manager = BluetoothManager()
         self._register_routes()
 
+    def login(self):
+        def decorator(f):
+            @wraps(f)
+            def decorated_function(*args, **kwargs):
+                print("Session authenticated =", session.get("authenticated"))
+                if session.get("authenticated") != True:
+                    return redirect(url_for("auth_handler"))
+                return f(*args, **kwargs)
+            return decorated_function
+        return decorator
+
     def _register_routes(self):
+        @self.app.route("/login", methods = ["GET", "POST"])
+        def auth_handler():
+            error = None
+            if request.method == "POST":
+                if request.form["password"] == BluetoothManager.PASSWORD:
+                    session["authenticated"] = True
+                    return redirect(url_for("index"))
+                else:
+                    error = "Access Denied: Incorrect Code!"
+            return render_template("login.html", error = error)
+
+        @self.app.route("/logout")
+        def logout():
+            session.clear()
+            return redirect(url_for("auth_handler"))
+
         @self.app.route("/")
+        @self.login()
         def index():
             return render_template("index.html")
 
         @self.app.route("/scan", methods = ["GET"])
+        @self.login()
         def scan():
             future = self.bluetooth_manager.run_async(self.bluetooth_manager.scan())
             devices = future.result(timeout = 30) # Wait for 30 Seconds
             return jsonify(devices)
 
         @self.app.route("/connect", methods = ["POST"])
+        @self.login()
         def connect():
             data = request.get_json()
             if not data or "deviceAddress" not in data:
@@ -97,22 +134,22 @@ class RobotDriverApp:
                     else (jsonify({"status": "Failed"}), 400))
 
         @self.app.route("/disconnect", methods = ["GET"])
+        @self.login()
         def disconnect():
             future = self.bluetooth_manager.run_async(self.bluetooth_manager.disconnect())
-            future.result(timeout = 10) # Wait for 5 Seconds
+            future.result(timeout = 10) # Wait for 10 Seconds
             return jsonify({"status": "Disconnected"})
 
         @self.app.route("/command", methods = ["POST"])
+        @self.login()
         def command():
             data = request.get_json()
             if not data or "command" not in data:
                 return jsonify({"error": "No Command Provided"}), 400 # Bad Request
-
             future = self.bluetooth_manager.run_async(
                 self.bluetooth_manager.send_cmd(data["command"])
             )
-            success, message = future.result(timeout = 10) # Wait for 5 Seconds
-
+            success, message = future.result(timeout = 10) # Wait for 10 Seconds
             if success:
                 return jsonify({"status": "OK", "msg": message})
             else:
@@ -122,4 +159,5 @@ class RobotDriverApp:
         self.app.run(host = "0.0.0.0", port = 5000, debug = False)
 
 if __name__ == "__main__":
+    load_dotenv() # Load Environment Variables from .env file
     RobotDriverApp().run()
