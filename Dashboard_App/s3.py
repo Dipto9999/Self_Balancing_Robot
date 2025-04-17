722b36db731f591c51906d1dfbb47e55bdbe73ac
Muntakim Rahman <muntakim.rahman@gmail.com>
2025-04-16 23:00:52 -0700

    Updating S3.py

--- Dashboard_App/s3.py
+++ Dashboard_App/s3.py
@@ -38,8 +38,8 @@ class S3Manager:
 
 if __name__ == "__main__":
     s3_manager = S3Manager()
-    # s3_manager.upload_files(s3_manager.figures_dir)
-    # s3_manager.upload_files(s3_manager.logbook_dir)
+    s3_manager.upload_files(s3_manager.figures_dir)
+    s3_manager.upload_files(s3_manager.logbook_dir)
     # s3_manager.upload_files(s3_manager.snapshot_dir)
     # s3_manager.upload_files(s3_manager.video_dir)
-    s3_manager.download_files()
+    # s3_manager.download_files()
